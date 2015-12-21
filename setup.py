54448a761d542ad4195f955ed726cb1e8903a3d4
Andreas Kaiser <disko@binary-punks.com>
2015-12-21 12:42:29 +0100

    Change CHANGES.txt and README.txt to *.rst to make them render on Github.

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
+with open(os.path.join(here, 'CHANGES.rst')) as f:
     CHANGES = f.read()
 
 requires = [
     'pyramid',
     'pycallgraph',
-    ]
+]
 
 setup(
     name='pyramid_pycallgraph',
